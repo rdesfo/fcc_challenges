b93f8bd883f1d4e8d8ad3c54b59d7f502bd99a8b
Ryan Desfosses <ryan@desfo.org>
2016-08-31 02:46:40 +0000

    created tests for Sum All Primes challenge

---
+++ src/SumAllPrimes.js
@@ -0,0 +1,19 @@
+
+// > typeof sumPrimes(10)
+// 'number'
+// > sumPrimes(10) 
+// 17
+// > sumPrimes(977)
+// 73156
+
+// sumPrimes :: Int -> Int
+function sumPrimes(i){
+  return i;
+}
+
+// isPrime :: Int -> Bool
+// > isPrime(17)
+// false
+function isPrime(i){
+
+}
