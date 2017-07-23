a980f8acf2381eee464aea2aca88586b9925aea4
Ryan Desfosses <ryan@desfo.org>
2017-07-23 11:25:22 -0400

    arr -> arr[0]

--- src/SmallestCommonMultiple.js
+++ src/SmallestCommonMultiple.js
@@ -1,13 +1,13 @@
 
 // > smallestCommons([1,5])
 // 60
-// > smallestCommons([5, 1])
+// > smallestCommons([5,1])
 // 60
-// > smallestCommons([1, 13])
+// > smallestCommons([1,13])
 // 360360
-// > smallestCommons([23, 18])
+// > smallestCommons([23,18])
 // 6056820
 
 function smallestCommons(arr) {
-  return arr;
+    return arr[0];
 }
