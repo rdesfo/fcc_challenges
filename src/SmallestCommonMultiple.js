06d3d9333e9d1fc047569f56cd2e014a6a6a48e1
Ryan Desfosses <ryan@desfo.org>
2017-07-27 15:58:40 -0400

    completed Smallest Common Multiple challenge

--- src/SmallestCommonMultiple.js
+++ src/SmallestCommonMultiple.js
@@ -10,14 +10,37 @@
 
 function range(x,y){
   var arr = [];
-  while (x < y){
+  while (x < y+1){
       arr.push(x++);
   }
     return arr;
 }
 
 function smallestCommons(arr) {
-  return range(arr[0], arr[1]);
+  const x = arr[0];
+  const y = arr[1];
+
+  if (x < y) {
+    return findCommonMultiple(range(x, y));
+  } else {
+    return findCommonMultiple(range(y, x));
+  }
+}
+
+// last one is goint to be greatest number
+// FindCommonMultiple :: [Int] -> Int
+function findCommonMultiple(r){
+  var m = r[r.length-1];
+  const n = m;
+
+  function divByM (x){
+    return m % x === 0;
+  }
+
+  while (r.every(divByM) == false){
+    m = m + n;
+  }
+  return m;
 }
 
 console.log(smallestCommons([1,5]));
